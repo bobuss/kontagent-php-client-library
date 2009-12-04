80cf021328f8c3d1a95e117475ab10803159df94
Lih Chen <dafreak@gibbon.(none)>
2009-12-04 11:26:18 -0800

    get rid of the extraneous keyword this

--- an_lib/kt_common.js
+++ an_lib/kt_common.js
@@ -179,7 +179,7 @@ function http_build_query( formdata, numeric_prefix, arg_separator ) {
             }
             return tmp.join(arg_separator);
         } else if(typeof(val) != "function") {
-            return this.urlencode(key) + "=" + this.urlencode(val);
+	  return urlencode(key) + "=" + urlencode(val);
         }
     };
 
